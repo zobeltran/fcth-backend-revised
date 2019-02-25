d64d5b272e8c5f37589b83fecb75fe31d5f20ec2
Renzo Beltran <rnzbeltran@gmail.com>
2019-02-25 14:51:13 +0800

    Added package api to namespace

--- app/src/routes/api_v1.py
+++ app/src/routes/api_v1.py
@@ -4,6 +4,7 @@ from app.src.helpers.decorators import authentication
 from app.api.controller.user import api as user_api
 from app.api.controller.ticket import api as ticket_api
 from app.api.controller.hotel import api as hotel_api
+from app.api.controller.package import api as package_api
 
 api_routes = Blueprint('api', __name__, url_prefix='/api/v1')
 api = Api(api_routes, title='First Choice Travel Hub API', 
@@ -12,3 +13,4 @@ api = Api(api_routes, title='First Choice Travel Hub API',
 api.add_namespace(user_api)
 api.add_namespace(ticket_api)
 api.add_namespace(hotel_api)
+api.add_namespace(package_api)
