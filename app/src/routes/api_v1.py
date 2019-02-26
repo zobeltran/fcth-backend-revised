3bb905e1349d9678905e858bc94a8ff7edeae21b
Renzo Beltran <rnzbeltran@gmail.com>
2019-02-27 01:51:44 +0800

    Added booking_api namespace

--- app/src/routes/api_v1.py
+++ app/src/routes/api_v1.py
@@ -5,6 +5,7 @@ from app.api.controller.user import api as user_api
 from app.api.controller.ticket import api as ticket_api
 from app.api.controller.hotel import api as hotel_api
 from app.api.controller.package import api as package_api
+from app.api.controller.booking import api as booking_api
 
 api_routes = Blueprint('api', __name__, url_prefix='/api/v1')
 api = Api(api_routes, title='First Choice Travel Hub API', 
@@ -14,3 +15,4 @@ api.add_namespace(user_api)
 api.add_namespace(ticket_api)
 api.add_namespace(hotel_api)
 api.add_namespace(package_api)
+api.add_namespace(booking_api)
