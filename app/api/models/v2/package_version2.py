cd5fad67d6402abbae797e9b7393289a8c58c2b6
Renzo Beltran <renzobeltran@Renzos-MacBook-Pro.local>
2019-03-24 13:56:24 +0800

    Added package version 2 models

---
+++ app/api/models/v2/package_version2.py
@@ -0,0 +1,72 @@
+"""
+Package Version 2 API Models
+"""
+from flask_restplus import Namespace, fields
+
+API = Namespace('Packages', 'Package related APIs', path='/packages')
+
+
+A_TIMESTAMP = API.model('Timestamp',
+                        {'dateCreated': fields.DateTime(),
+                         'dateUpdated': fields.DateTime()
+                         })
+
+A_TICKET_DETAILS = API.model('Ticket Package Details',
+                             {'id': fields.Integer(),
+                              'flightNo': fields.String(),
+                              'origin': fields.String(),
+                              'destination': fields.String()
+                              })
+
+A_HOTEL_DETAILS = API.model('Hotel Pakage Details',
+                            {'id': fields.Integer(),
+                             'name': fields.String(),
+                             'roomType': fields.String(),
+                             'capacity': fields.Integer()
+                             })
+
+A_PACKAGE_SPECIFICATION = API.model('Package Specifications',
+                                    {'days': fields.String(),
+                                     'notes': fields.String(),
+                                     'date': fields.Date()
+                                     })
+
+A_PACKAGE_PRICES = API.model('Package Prices',
+                             {'ticket': fields.Float(),
+                              'hotel': fields.Float(),
+                              'serviceCharge': fields.Float(),
+                              'vat': fields.Float()})
+
+A_PACKAGE_SPECS = API.model('Package Specifications',
+                            {'days': fields.String(),
+                             'notes': fields.String(),
+                             'date': fields.Date()
+                            })
+
+A_ITINERARY = API.model('Itinerary',
+                        {'id': fields.Integer(),
+                         'itinerary': fields.String()})
+
+A_PACKAGE = API.model('Package Details',
+                      {'id': fields.Integer(),
+                       'name': fields.String(),
+                       'price': fields.Nested(A_PACKAGE_PRICES),
+                       'departureDate': fields.Date(),
+                       'itinerary': fields.List(fields.Nested(A_ITINERARY)),
+                       'details': fields.Nested(A_PACKAGE_SPECIFICATION),
+                       'ticket': fields.Nested(A_TICKET_DETAILS),
+                       'hotel': fields.Nested(A_HOTEL_DETAILS),
+                       'remainingSlots': fields.Integer(),
+                       'expirationDate': fields.Date(),
+                       'isExpired': fields.Boolean()
+                      })
+
+A_CREATE_PACKAGE_NEW = API.model('Create Package',
+                                 {'name': fields.String(),
+                                  'details': fields.Nested(A_PACKAGE_SPECS),
+                                  'itinerary':fields.List(fields.String(), description='itinerary'),
+                                  'ticket': fields.Integer(),
+                                  'hotel': fields.Integer(),
+                                  'remainingSlots': fields.Integer(),
+                                  'expirationDate': fields.Date()
+                                 })
